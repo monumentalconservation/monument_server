e963d56b4f0e6c871f249fd606fd417355c988d6
Rosie Brigham <rosiebrigham@gmail.com>
2019-07-23 21:54:13 +0100

    more probing - does not seem to be this either

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -14,6 +14,32 @@ module ApplicationHelper
     end
   end
 
+  # Helper copied from the smart_listing gem to see if it is to blame - it does not seem to be
+  def smart_listing_foo *args
+    options = args.extract_options!
+    name = (args[0] || options[:name] || controller_name).to_sym
+    smart_listing = @smart_listings[name]
+
+    # don't update list if params are missing (prevents interfering with other lists)
+    if params.keys.select{|k| k.include?("smart_listing")}.present? && !params[smart_listing.base_param]
+      return unless options[:force]
+    end
+
+    builder = SmartListing::Helper::Builder.new(name, smart_listing, self, {}, nil)
+    # binding.pry
+    render(:partial => 'smart_listing/update_list', :locals => {
+      :name => smart_listing.name,
+      :part => smart_listing.partial,
+      :smart_listing => builder,
+      :smart_listing_data => {
+        smart_listing_config.data_attributes(:params) => smart_listing.all_params,
+        smart_listing_config.data_attributes(:max_count) => smart_listing.max_count,
+        smart_listing_config.data_attributes(:item_count) => smart_listing.count,
+      },
+      :locals => options[:locals] || {}
+    })
+  end
+
   def admin?
     current_user.try(:admin?)
   end
