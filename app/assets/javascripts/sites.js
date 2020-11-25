a3abdfccbc2611c7a90833e33b4933015b7e164a
Rosie-Brigham <rosiebrigham@gmail.com>
2020-11-25 15:36:45 +0000

    adds modal on backend

--- app/assets/javascripts/sites.js
+++ app/assets/javascripts/sites.js
@@ -3,9 +3,14 @@ core.Sites = {};
 $( document ).on('turbolinks:load', function() {
   if ($('h3#site-info').size() > 0) {
     core.Sites.readySiteSubmissionTags()
+    core.Sites.readyModal()
   } 
 });
 
 core.Sites.readySiteSubmissionTags = function() {
   $('.tags').tagsInput();
+}
+
+core.Sites.readyModal = function() {
+  MicroModal.init();
 }
