9ad1ce97a326bdda487c2e1885c7ebb005449e6d
Rosie-Brigham <rosiebrigham@gmail.com>
2022-04-06 13:12:36 +0100

    hides unsorted from front end

--- app/models/site.rb
+++ app/models/site.rb
@@ -2,5 +2,5 @@ class Site < ApplicationRecord
   has_many :submissions
   belongs_to :site_group
   
-  UNSORTED_SITES = ['Instagram unsorted', 'Twitter unsorted', 'Unsuitable'].freeze
+  UNSORTED_SITES = ['Instagram unsorted', 'Twitter unsorted', 'Unsuitable', "Unsorted"].freeze
 end
